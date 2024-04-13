6f140028455e3d92bf0e6431b76afc5657c6e59f
Abby Suarez <abby@mythril.co>
2024-04-13 14:53:36 -0700

    refactor catchable, lock mouse

--- Catchable.gd
+++ Catchable.gd
@@ -1,19 +1,42 @@
 class_name Catchable extends Sprite2D
 
-@onready var timer: Timer = $Timer
+@onready var decision_timer: Timer = $Timer
 
-var type: int = 0;
+enum Type {BUG, YARN, CACTUS, TRAP};
+
+const MAP_SIZE = 512;
+
+var type: int = Type.BUG;
 var direction: Vector2 = Vector2(randf(), randf());
-var speed: int = randi_range(1.5, 2.5);;
+var speed: float = randf_range(1.5, 2.5);
+var default_points: int = 0;
 
+func get_points() -> int:
+	match type:
+		Type.BUG:
+			# distance to center of game - fubar
+			return int(position.distance_to(Vector2(MAP_SIZE/2, MAP_SIZE/2)) *.5);
+		_:
+			return default_points;
 
 func _ready() -> void:
-	timer.timeout.connect(handle_timer)
-	timer.start();
+	decision_timer.timeout.connect(handle_timer)
+	decision_timer.start();
+
+	match type:
+		Type.YARN:
+			default_points = 50;
+			direction = Vector2(randf_range(-1, 1), randf_range(-1, 1));
+			speed = randf_range(4, 6);
 	return;
 
+func _process(delta: float) -> void:
+	position += direction * speed;
+	return;
 
 func handle_timer() -> void:
-	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1));
-	speed = randi_range(2.5, 3.5);
+	match type:
+		Type.BUG:
+			direction = Vector2(randf_range(-1, 1), randf_range(-1, 1));
+			speed = randf_range(2.5, 3.5);
 	return;
--- main.gd
+++ main.gd
@@ -10,7 +10,7 @@ var isGrabbing = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN);
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN);
 	pass # Replace with function body.
 
 func _input(event: InputEvent) -> void:
